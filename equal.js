f60c750850157a52bdf019edd401e5642c622be8
Vasianandcarps <90087480+Vasianandcarps@users.noreply.github.com>
2024-04-17 20:55:35 +0300

    Update equal.js
    rotation

--- equal.js
+++ equal.js
@@ -1,40 +1,51 @@
-window.addEventListener('load', function() {
-  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    screen.orientation.lock("portrait");
-  }
 
-  let start = document.getElementById("start");
+  let start=document.getElementById("start");
 
-  start.onclick = function solveQuadratic() {
-    let a = parseFloat(document.getElementById("in_a").value);
-    let b = parseFloat(document.getElementById("in_b").value);
-    let c = parseFloat(document.getElementById("in_c").value);
+start.onclick= function solveQuadratic() {
+  let a = parseFloat(document.getElementById("in_a").value);
+  let b = parseFloat(document.getElementById("in_b").value);
+  let c = parseFloat(document.getElementById("in_c").value);
 
-    if (a != 0) {
-      let D = b * b - 4 * a * c;
+  if (a != 0) {
+    let D = b * b - 4 * a * c;
+    
+    if (D >= 0) {
+      let o = -b + Math.sqrt(D);
+      let o1 = -b - Math.sqrt(D);
+      let a1 = 2 * a;
+      let x1 = o / a1;
+      let x2 = o1 / a1;
 
-      if (D >= 0) {
-        let o = -b + Math.sqrt(D);
-        let o1 = -b - Math.sqrt(D);
-        let a1 = 2 * a;
-        let x1 = o / a1;
-        let x2 = o1 / a1;
-
-        document.getElementById("answer").innerHTML = "D=" + D + ";" + "x1=" + x1 + ";" + "x2=" + x2;
-      } else {
-        document.getElementById("answer").innerHTML = "D cannot be less than 0";
-      }
+      document.getElementById("answer").innerHTML = "D=" + D + ";" +"x1=" + x1 + ";" + "x2=" + x2;
     } else {
-      if (b != 0) {
-        let x = -c / b;
-        document.getElementById("answer").innerHTML = "x=" + x;
+      document.getElementById("answer").innerHTML = "D cannot be less than 0";
+    }
+  } else {
+    if (b != 0) {
+      let x = -c / b;
+      document.getElementById("answer").innerHTML = "x=" + x;
+    } else {
+      if (c == 0) {
+        document.getElementById("answer").innerHTML = "Infinite solutions";
       } else {
-        if (c == 0) {
-          document.getElementById("answer").innerHTML = "Infinite solutions";
-        } else {
-          document.getElementById("answer").innerHTML = "No real solutions";
-        }
+        document.getElementById("answer").innerHTML = "No real solutions";
       }
     }
   }
+}
+
+window.addEventListener('orientationchange', function() {
+  var orientation = Math.abs(window.orientation) == 90 ? 'landscape' : 'portrait';
+
+  if (orientation === 'portrait') {
+    // Применяем стили для блокировки поворота
+    document.documentElement.style.overflow = 'hidden';
+    document.body.style.overflow = 'hidden';
+  } else {
+    // Удаляем стили блокировки поворота
+    document.documentElement.style.overflow = '';
+    document.body.style.overflow = '';
+  }
 }, false);
+
+
